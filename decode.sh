3e0cc6c68765252164f337445895146c04f66d14
Bitcion <threebitcoin@icloud.com>
2023-02-18 20:22:55 +0800

    Update decode.sh

--- decode.sh
+++ decode.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# 获取 GFWList 并解码
-curl -sL "https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt" | base64 -d > gfwlist.txt
+# Download and decode the GFWList
+wget -qO- --no-check-certificate https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt | base64 -d > gfwlist.txt
 
-# 转换 GFWList 格式为 IPv6.txt
-cat gfwlist.txt | grep -v "^\!" | grep -v "^@" | sed -r 's/(.*)/\|\|\1^$\|~CNAME/' > ipv6.txt
+# Modify each address in GFWList to the desired format
+sed 's/^/||/' gfwlist.txt | sed 's/$/^$dnstype=~A|~CNAME/' > ipv6.txt
 
-# 提交文件到仓库并推送到 GitHub
+# Push the changes to GitHub
 git add ipv6.txt
-git commit -m "Add IPv6.txt file"
+git commit -m "Update ipv6.txt"
 git push origin main
