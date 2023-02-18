a671b9d0e528af0c9e04d8f03270f3c1a891126a
Bitcion <threebitcoin@icloud.com>
2023-02-18 11:36:25 +0000

    错误

--- decode.sh
+++ decode.sh
@@ -4,7 +4,7 @@
 curl -sL "https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt" | base64 -d > gfwlist.txt
 
 # 转换 GFWList 格式为 IPv6.txt
-cat gfwlist.txt | grep -v "^\!" | grep -v "^@" | sed -r 's/(.*)/\|\|\1^$\|~CNAME/' > ipv6.txt
+cat gfwlist.txt | grep -v "^\!" | grep -v "^@" | sed -r 's/\^/\$dnstype=~A|~CNAME\&/' > ipv6.txt
 
 # 提交文件到仓库并推送到 GitHub
 git add ipv6.txt
