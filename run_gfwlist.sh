6bcacb468c76d37c4bfb30429ea8d9f9afab1780
Bitcion <threebitcoin@icloud.com>
2024-05-19 03:16:29 +0800

    Update run_gfwlist.sh

--- run_gfwlist.sh
+++ run_gfwlist.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# 删除旧的ipv6.txt文件
+# 删除旧的ipv6.txt和ipv4.txt文件
 rm -f ./ipv6.txt
+rm -f ./ipv4.txt
 
 # 下载gfwlist2dnsmasq.sh脚本
 wget https://raw.githubusercontent.com/cokebar/gfwlist2dnsmasq/master/gfwlist2dnsmasq.sh
@@ -11,14 +12,16 @@ chmod +x gfwlist2dnsmasq.sh
 sh gfwlist2dnsmasq.sh -l -o ./gfwlist_domains.txt
 
 # 将域名列表转换成dnsmasq规则并存储到ipv6.txt文件中
-sed 's/.*/\|\|&\^\$dnstype\=~A\|~CNAME/' ./gfwlist_domains.txt > ./ipv6.txt
+sed 's/.\*/\\|\\|&\\^\\$dnstype\\=~A\\|~CNAME/' ./gfwlist_domains.txt > ./ipv6.txt
 
 # 删除下载的脚本和域名列表文件
 rm -f gfwlist2dnsmasq.sh
 rm -f ./gfwlist_domains.txt
 
-# 下载anti-ad-for-smartdns.conf文件
-wget https://anti-ad.net/anti-ad-for-smartdns.conf
-
-# 将文件里的/#改成/#4
-sed -i 's/#\//#4\//' anti-ad-for-smartdns.conf
+# 从AdGuard规则.txt提取域名到ipv4.txt
+while read -r line; do
+    if [[ $line =~ ^[|]+([a-zA-Z0-9.-]+)[|]+[^|]+[|]+[|]+\^(\$dnstype=~A[|]+~CNAME) ]]; then
+        domain=${BASH_REMATCH[1]}
+        echo "$domain" >> ipv4.txt
+    fi
+done < "AdGuard规则.txt"
