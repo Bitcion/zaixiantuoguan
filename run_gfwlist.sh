bff2fa3bb112b12fec90a663c1df9e7a8a8fe768
Bitcion <threebitcoin@icloud.com>
2024-06-01 16:28:36 +0800

    Update run_gfwlist.sh

--- run_gfwlist.sh
+++ run_gfwlist.sh
@@ -5,6 +5,6 @@ rm -f ./AdGuard.txt
 
 # 从 ipv4.txt 中读取域名并转换为 AdGuard 规则格式,写入 AdGuard.txt
 while read -r line; do
-    rule="||$line^$dnstype=~A|~CNAME"
+    rule="||$line^\$dnstype=~A|~CNAME"
     echo "$rule" >> AdGuard.txt
 done < ipv4.txt
