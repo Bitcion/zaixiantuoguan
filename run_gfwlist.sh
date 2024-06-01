1652a01a3f1c6ca21ca81daceffab4d08da58f49
Bitcion <threebitcoin@icloud.com>
2024-06-01 15:47:13 +0800

    Update run_gfwlist.sh

--- run_gfwlist.sh
+++ run_gfwlist.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# 定义DNS类型
-dnstype="A,CNAME"
-
 # 删除旧的AdGuard.txt文件
 rm -f ./AdGuard.txt
 
 # 从ipv4.txt中读取域名并转换为Adguard规则格式,写入AdGuard.txt
 while read -r line; do
-    rule="||$line^$dnstype=A|CNAME"
+    # 直接写入$dnstype变量的文字
+    rule="||$line^\\\$dnstype=~A|~CNAME"
     echo "$rule" >> AdGuard.txt
 done < ipv4.txt
