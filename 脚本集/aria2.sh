e26e0c22caa7362f51b2a4b6189edbcfa66083b3
Bitcion <threebitcoin@icloud.com>
2025-04-20 02:32:40 +0800

    Create aria2.sh

---
+++ 脚本集/aria2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+/usr/bin/aria.sh stop
+list=`wget -qO- https://cf.trackerslist.com/all_aria2.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
+echo ${list}
+if [ -z "`grep "bt-tracker" /etc/storage/aria2_conf.sh`" ]; then
+    sed -i '$a bt-tracker='${list} /etc/storage/aria2_conf.sh
+    echo 添加"bt-tracker="前缀...
+else
+    sed -i "s@bt-tracker.*@bt-tracker=$list@g" /etc/storage/aria2_conf.sh
+    echo 升级完成...
+fi
+/usr/bin/aria.sh restart
+
