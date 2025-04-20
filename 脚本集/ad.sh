f015747e8e3d47f22fd855e1000efaac03721628
Bitcion <threebitcoin@icloud.com>
2025-04-20 12:49:13 +0800

    Update ad.sh

--- 脚本集/ad.sh
+++ 脚本集/ad.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-
 # 定义一个函数，用于下载和处理文件
 download_and_process() {
     # 接收三个参数，分别是下载的URL，保存的文件路径，和处理的命令
     URL=$1
     OUTPUT_FILE=$2
     PROCESS_CMD=$3
-
     # 下载文件并保存到临时文件
     TMP_FILE="/tmp/$(basename $OUTPUT_FILE)_temp.txt"
     wget -O "$TMP_FILE" "$URL"
-
     # 检查文件是否下载成功
     if [ $? -eq 0 ]; then
         # 根据处理命令，提取并重新生成列表，保存到目标文件
@@ -19,16 +16,24 @@ download_and_process() {
     else
         echo "Failed to download the file from $URL."
     fi
-
     # 删除临时文件
     rm -f "$TMP_FILE"
 }
 
-# 下载和处理cn.txt文件，不需要额外处理
+# 第一部分：下载和处理各种文件
 download_and_process "https://bitcion.github.io/zaixiantuoguan/cn.txt" "/opt/cn.txt" "cat"
-
-# 下载和处理DLC.txt文件，不需要额外处理
 download_and_process "https://bitcion.github.io/zaixiantuoguan/ipv4.txt" "/opt/quic.txt" "cat"
-
-# 下载和处理anti-ad-for-smartdns.conf文件，替换/#为/#4
 download_and_process "https://anti-ad.net/anti-ad-for-smartdns.conf" "/opt/anti-ad-for-smartdns.conf" "sed '/^[^#]/s/\/#/\/#4/g'"
+
+# 第二部分：更新aria2的BT追踪器列表
+/usr/bin/aria.sh stop
+list=`wget -qO- https://cf.trackerslist.com/best.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
+echo ${list}
+if [ -z "`grep "bt-tracker" /etc/storage/aria2_conf.sh`" ]; then
+    sed -i '$a bt-tracker='${list} /etc/storage/aria2_conf.sh
+    echo 添加"bt-tracker="前缀...
+else
+    sed -i "s@bt-tracker.*@bt-tracker=$list@g" /etc/storage/aria2_conf.sh
+    echo 升级完成...
+fi
+/usr/bin/aria.sh restart
