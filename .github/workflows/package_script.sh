e595600082b8a98c0c57d2109f184fc908e885c8
Bitcion <threebitcoin@icloud.com>
2025-11-11 15:29:21 +0800

    Update package_script.sh

--- .github/workflows/package_script.sh
+++ .github/workflows/package_script.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
-
-# 定义输入和输出文件
 INPUT_FILE="gfwDLC.txt"
 OUTPUT_FILE="ipv4.txt"
 
-# 如果输出文件已存在，先删除它
-if [ -f "$OUTPUT_FILE" ]; then
-    rm "$OUTPUT_FILE"
-fi
+[ ! -f "$INPUT_FILE" ] && exit 1
+[ ! -f "$OUTPUT_FILE" ] && touch "$OUTPUT_FILE"
 
-# 处理文件内容，移除每行开头的"+."并写入输出文件
-while IFS= read -r line || [ -n "$line" ]; do
-    # 如果行以"+."开头，则移除这个前缀
-    if [[ $line == +.* ]]; then
-        echo "${line:2}" >> "$OUTPUT_FILE"
-    else
-        # 不以"+."开头的行直接写入
-        echo "$line" >> "$OUTPUT_FILE"
-    fi
-done < "$INPUT_FILE"
+sed 's/^+\.//' "$INPUT_FILE" | sort -u > /tmp/new_sorted.txt
+sort -u "$OUTPUT_FILE" > /tmp/old_sorted.txt
+comm -23 /tmp/new_sorted.txt /tmp/old_sorted.txt >> "$OUTPUT_FILE"
+rm /tmp/new_sorted.txt /tmp/old_sorted.txt
